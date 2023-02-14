b61bf7358905b660157abe5bc58cc93c024fc178
sondang <nostarwhere.pc@gmail.com>
2023-02-14 21:32:51 +0700

    new header

--- src/components/general/AboutMe.js
+++ src/components/general/AboutMe.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import avatar from '../../assets/images/avatar2.jpg';
+import avatar from '../../assets/images/avatar.jpeg';
+import './AboutMe.scss';
+import aboutMe from "../../assets/images/aboutme.jpeg";
+import aboutMe1 from "../../assets/images/aboutme1.jpeg";
+import aboutMe2 from "../../assets/images/aboutme2.jpeg";
+import aboutMe4 from "../../assets/images/aboutme4.jpeg";
+import aboutMe5 from "../../assets/images/aboutme5.jpeg";
+import aboutMe6 from "../../assets/images/aboutme6.jpeg";
+import aboutMe7 from "../../assets/images/aboutme7.jpeg";
 
 function AboutMe(props) {
   return (
-    <div className='flex bg-ghost-white'>
+    <div className='flex flex-col bg-ghost-white'>
       <div className="flex flex-col items-center container px-auto p-10">
         <img className='h-24 w-24 rounded-full mb-6' src={avatar} alt="" />
         <h3 className='text-2xl font-extrabold text-center mb-4'>Hi, I'm Black Valentine</h3>
@@ -14,6 +22,30 @@ function AboutMe(props) {
           <div className="w-6 h-6 rounded-full bg-cherenkov-radiation"></div>
           <div className="w-6 h-6 rounded-full bg-youtube-red"></div>
           <div className="w-6 h-6 rounded-full bg-strong-red"></div>
+        </div>
+      </div>
+
+      <div className="about__layout">
+        <div className="about__layout-item">
+          <img src={aboutMe2} alt="" className="about__layout-item" />
+        </div>
+        <div className="about__layout-item">
+          <img src={aboutMe1} alt="" className="about__layout-item" />
+        </div>
+        <div className="about__layout-item">
+          <img src={aboutMe} alt="" className="about__layout-item" />
+        </div>
+        <div className="about__layout-item">
+          <img src={aboutMe4} alt="" className="about__layout-item" />
+        </div>
+        <div className="about__layout-item">
+          <img src={aboutMe5} alt="" className="about__layout-item" />
+        </div>
+        <div className="about__layout-item">
+          <img src={aboutMe6} alt="" className="about__layout-item" />
+        </div>
+        <div className="about__layout-item">
+          <img src={aboutMe7} alt="" className="about__layout-item" />
         </div>
       </div>
     </div>
--- src/components/general/Header.js
+++ src/components/general/Header.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 import SvgLogo from './SvgLogo';
 
 function Header(props) {
+  const [lightMode, setLightMode] = useState(true)
   return (
-    <div className='flex'>
-      <div className="m-auto">
-        <SvgLogo />
+    <div className="container grid grid-cols-3 m-auto">
+      <SvgLogo />
+      <nav>
+        <ul className='flex items-center gap-4'>
+          <li className='font-bold'>About me</li>
+          <li className='font-bold'>Login</li>
+          <li className='font-bold'>Blogs</li>
+          <li className='font-bold'>404 Page</li>
+        </ul>
+      </nav>
+
+      <div className="flex items-center">
+        <div className="group mr-2">
+          <div
+            className="w-8 h-8 flex border border-solid border-light-gray rounded-full cursor-pointer group-hover:bg-pastel-red"
+            onClick={() => setLightMode(!lightMode)}
+          >
+            {lightMode ? <i className="fas fa-sun m-auto group-hover:text-white"></i> : <i className="fas fa-moon m-auto group-hover:text-white"></i>}
+          </div>
+        </div>
+        <div className="group">
+          <div className="w-8 h-8 flex border border-solid border-light-gray rounded-full cursor-pointer group-hover:bg-pastel-red">
+            <i className="fas fa-search m-auto group-hover:text-white"></i>
+          </div>
+        </div>
       </div>
     </div>
   );
